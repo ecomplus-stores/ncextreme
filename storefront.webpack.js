9602ebbbd12280625f97a51c0d38a07c1b3e092d
matheusgnreis <matheus@e-com.club>
2023-05-02 14:06:39 -0300

    chore: update store

---
+++ storefront.webpack.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html')
+    }
+  }
+})
