cabbb4ac753792561d697a0cd886ee041ca2d9ea
matheusgnreis <matheus@e-com.club>
2023-06-20 12:06:50 -0300

    chore: update image product

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,7 +4,10 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
-      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html'),
+      './js/ProductGallery.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductGallery.js'),
+      './scss/ProductGallery.scss': path.resolve(__dirname, 'template/js/custom-js/scss/ProductGallery.scss')
     }
   }
 })
