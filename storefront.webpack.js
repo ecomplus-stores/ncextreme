4ba84a115aa502c5e17d9eda2aae896e9ab881cc
matheusgnreis <matheus@e-com.club>
2023-08-23 18:08:42 -0300

    chore: update sections

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,7 +7,8 @@ module.exports = () => ({
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html'),
       './js/ProductGallery.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductGallery.js'),
-      './scss/ProductGallery.scss': path.resolve(__dirname, 'template/js/custom-js/scss/ProductGallery.scss')
+      './scss/ProductGallery.scss': path.resolve(__dirname, 'template/js/custom-js/scss/ProductGallery.scss'),
+      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config')
     }
   }
 })
